465fb25b68313abd386dffd8c99beb2a99d5db2d
NorweskiDrwal <mateuszmsas@gmail.com>
2018-03-30 15:10:20 +0200

    Another commit

--- src/common/Header.js
+++ src/common/Header.js
@@ -4,7 +4,6 @@ import LoginForm from '../components/LoginForm';
 
 const header = (props) => (
 	<header>
-		<LoginForm />
 	</header>
 );
 
--- src/common/Main.js
+++ src/common/Main.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
+import ContentCard from '../components/ContentCard';
+import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 
 const main = (props) => (
 	<main>
 		<Container fluid>
 			<Row>
-				<Col xs="12" md="7"></Col>
+				<Col xs="12" md="7">
+					<ContentCard />
+				</Col>
 				<Col xs="12" md="4">
+					<LoginForm />
 					<RegisterForm />
 				</Col>
 				<Col xs="12" md="1"></Col>
